2f93ccdc1e448c05d9ec54c20bf1ba4e5b82691a
Paul <247144100+paulperrin-stack@users.noreply.github.com>
2026-03-17 14:16:30 +0700

    feat: add entry point, HTML structure and CSS styles

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('Battleship loading...');
+import Game from './Game';
+import DOM from './DOM';
+import './styles.css';
+
+const game = Game();
+game.setupBoards();
+
+const dom = DOM(game);
+game.registerUpdateCallback(dom.render);
+
+dom.render();
